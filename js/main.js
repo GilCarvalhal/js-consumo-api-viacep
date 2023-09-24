d0d65c49f460472e41b4a6b7126257f0a4961aa5
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-09-24 10:21:53 -0300

    fetch API

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 
 const pesquisarCep = () => {
     const cep = document.getElementById('cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`
-    console.log(cep);
+    const url = `http://viacep.com.br/ws/${cep}/json/`;
+    fetch(url).then(response => response.json()).then(console.log);
 }
 
 document.getElementById('cep').addEventListener('focusout', pesquisarCep);
