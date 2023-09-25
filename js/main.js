58adb29df556ea580b892a086e45cba74fbaed8e
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-09-25 15:48:17 -0300

    async/await

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-'use strict';
+"use strict";
 
-const pesquisarCep = () => {
-    const cep = document.getElementById('cep').value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`;
-    fetch(url).then(response => response.json()).then(console.log);
-}
+const pesquisarCep = async () => {
+  const cep = document.getElementById("cep").value;
+  const url = `http://viacep.com.br/ws/${cep}/json/`;
 
-document.getElementById('cep').addEventListener('focusout', pesquisarCep);
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then(console.log());
+
+  const dados = await fetch(url);
+  const endereco = await dados.json();
+  console.log(endereco);
+};
+
+document.getElementById("cep").addEventListener("focusout", pesquisarCep);
