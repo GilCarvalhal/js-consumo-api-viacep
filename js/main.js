b7819d0508664454ecb1e84d4cebc60f999b5c6f
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-09-25 19:58:35 -0300

    preenchendo formulario

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 "use strict";
 
+const preencherFormulario = (endereco) => {
+  document.getElementById("endereco").value = endereco.logradouro;
+  document.getElementById("bairro").value = endereco.bairro;
+  document.getElementById("cidade").value = endereco.localidade;
+  document.getElementById("estado").value = endereco.uf;
+};
+
 const pesquisarCep = async () => {
   const cep = document.getElementById("cep").value;
   const url = `http://viacep.com.br/ws/${cep}/json/`;
@@ -10,7 +17,7 @@ const pesquisarCep = async () => {
 
   const dados = await fetch(url);
   const endereco = await dados.json();
-  console.log(endereco);
+  preencherFormulario(endereco);
 };
 
 document.getElementById("cep").addEventListener("focusout", pesquisarCep);
