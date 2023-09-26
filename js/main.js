1b9c8a087f48d8a8a236369eea650009824bc8c0
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-09-26 13:54:35 -0300

    validar cep

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 "use strict";
 
+const limparFormulario = (endereco) => {
+  document.getElementById("endereco").value = "";
+  document.getElementById("bairro").value = "";
+  document.getElementById("cidade").value = "";
+  document.getElementById("estado").value = "";
+};
+
 const preencherFormulario = (endereco) => {
   document.getElementById("endereco").value = endereco.logradouro;
   document.getElementById("bairro").value = endereco.bairro;
@@ -7,7 +14,13 @@ const preencherFormulario = (endereco) => {
   document.getElementById("estado").value = endereco.uf;
 };
 
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
 const pesquisarCep = async () => {
+  limparFormulario();
+
   const cep = document.getElementById("cep").value;
   const url = `http://viacep.com.br/ws/${cep}/json/`;
 
@@ -15,12 +28,16 @@ const pesquisarCep = async () => {
   //     .then((response) => response.json())
   //     .then(console.log());
 
-  const dados = await fetch(url);
-  const endereco = await dados.json();
-  if (endereco.hasOwnProperty("erro")) {
-    document.getElementById("endereco").value = "CEP não encontrado!";
+  if (cepValido(cep)) {
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    if (endereco.hasOwnProperty("erro")) {
+      document.getElementById("endereco").value = "CEP não encontrado!";
+    } else {
+      preencherFormulario(endereco);
+    }
   } else {
-    preencherFormulario(endereco);
+    document.getElementById("endereco").value = "CEP incorreto!";
   }
 };
 
